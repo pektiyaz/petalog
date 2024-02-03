12a28b07089b69a32846e678c7ce4b307baeb7e6
Pektiyaz Talibov <pektiyaztalibov@gmail.com>
2024-02-03 15:40:41 +0400

    memory limit

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
+ini_set('memory_limit', '-1');
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
